a61ac1c3581b33f6094180aa37c5089fbfd57ff6
Ashu Saifi <ashu2131995@gmail.com>
2025-11-12 00:10:45 +0530

    Refactor video controller and router: enhance error handling, add updatedVideo method, and improve file upload logic

--- Backend/src/controller/video.controller.js
+++ Backend/src/controller/video.controller.js
@@ -3,41 +3,53 @@ import { asyncHandler } from "../utils/aysncHandler.js";
 import { videoModel } from "../model/video.model.js";
 import uploadOnCloudinary from "../utils/cloudinary.js";
 import fs from "fs";
-import path from "path";
+import path from "node:path";
+import { ApiError } from "../utils/ApiError.js";
+
+// function to rename video file and move to temp folder and upload on cloudinary
+const videoFile =  async function videoFile(originalPath, newFileName) {
+    const videoExtention = path.extname(originalPath.path);
+    const newFileNameWithExt = `${newFileName + videoExtention}`;
+    const renamedFilePath = path.join("public/temp", newFileNameWithExt); 
+
+    fs.renameSync(originalPath.path, renamedFilePath);
+
+    const videoPath = await uploadOnCloudinary(renamedFilePath, "video");
+    if (!videoPath) {
+        throw new Error("Cannot upload video on cloudinary");
+    }
+    return videoPath.url;
+}
+
 
 
 const createVideo = asyncHandler(async (req, res) => {
     const videoName = req.body.videoName
     if (!videoName) {
-        res.status(400)
-        throw new Error("videoName is required")
+        res.status(400).json(new ApiError(400, "videoName is required"))
     }
 
     const videoFilesLocalPath = req.files?.videoFile[0];
     if (!videoFilesLocalPath) {
-        res.status(400)
-        throw new Error("videoFile is required")
+        res.status(400).json(new ApiError(400, "video file is required"))
     }
     
     // get extention
     const videoExtention = path.extname(videoFilesLocalPath.path)
     if (!videoExtention) {
-        res.status(400)
-        throw new Error("Invalid video file")
+        res.status(400).json(new ApiError(400, "Invalid video file"))
     }
     
     // rename file
     const renameVideo = `${videoName+videoExtention}`
     if (!renameVideo) {
-        res.status(500)
-        throw new Error("Cannot rename video file")
+        res.status(500).json(new ApiError(500, "Cannot rename video file"))
     }
     
     // move file to temp folder with new name
     const videoFilesLocalNewPath = path.join("public/temp", renameVideo)
     if (!videoFilesLocalNewPath) {
-        res.status(500)
-        throw new Error("Cannot move video file")
+        res.status(500).json(new ApiError(500, "Cannot move video file"))
     }
 
     // move file
@@ -46,22 +58,19 @@ const createVideo = asyncHandler(async (req, res) => {
     // upload on cloudinary
     const videoUrl = await uploadOnCloudinary(videoFilesLocalNewPath, "video")
     if (!videoUrl) {
-        res.status(500)
-        throw new Error("Cannot upload video on cloudinary")
+        res.status(500).json(new ApiError(500, "Cannot upload video on cloudinary"))
     }
 
     // thumbnail
     const thumbnailLocalPath = req.files?.thumbnail[0].path
     if (!thumbnailLocalPath) {
-        res.status(400)
-        throw new Error("thumbnail is required")
+        res.status(400).json(new ApiError(400, "thumbnail is required"))
     } 
 
     // upload thumbnail on cloudinary
     const thumbnailUrl = await uploadOnCloudinary(thumbnailLocalPath, "image")
     if (!thumbnailUrl) {
-        res.status(500)
-        throw new Error("Cannot upload thumbnail on cloudinary")
+        res.status(500).json(new ApiError(500, "Cannot upload thumbnail on cloudinary"))
     }
     
     // video title fields
@@ -89,4 +98,42 @@ const createVideo = asyncHandler(async (req, res) => {
     res.status(201).json(new ApiResponse(true, "Video created successfully", video))
 })
 
-export {createVideo}
+// updated video controller
+const updatedVideo = asyncHandler(async (req, res) => {
+   
+    //get video id from params
+    const videoId = req.params._id
+    if (!videoId) {
+        res.status(400).json(new ApiError(400, "Video id is required"))       
+    }
+
+    //get video name from body
+    const videoName = req.body.videoName
+
+    // get video file from req files
+    const videoFilesLocalPath = req.files?.videoFile[0];
+    if (!videoFilesLocalPath) {
+        res.status(400).json(new ApiError(400, "video file is required"))
+    }
+
+    // rename and move video file to temp folder and upload on cloudinary
+    const videoFilesLocalNewPath = await videoFile(videoFilesLocalPath, videoName)
+    const video = await videoModel.findByIdAndUpdate(videoId, {videoFile: videoFilesLocalNewPath})
+    if (!video) {
+        res.status(404).json(new ApiError(404, "Video not found"))
+    }
+    res.status(200).json(new ApiResponse(true, "Video updated successfully", video))
+})
+
+// delete video controller
+const deleteVideo = asyncHandler(async (req, res) => {
+})
+
+const getVideo = (req, res) => {
+    res.send("hellow world")
+}
+
+export {createVideo,
+    updatedVideo,
+    deleteVideo,
+    getVideo}
--- Backend/src/routes/video.router.js
+++ Backend/src/routes/video.router.js
@@ -1,16 +1,31 @@
 import { Router } from "express";
-import { createVideo } from "../controller/video.controller.js";
+import { createVideo, getVideo, updatedVideo} from "../controller/video.controller.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
 import { upload } from "../middleware/multer.middleware.js";
 
+const videoRouter = Router();
 
-const videoRouter = Router()
+videoRouter.post(
+  "/createvideo",
+  verifyJWT,
+  upload.fields([
+    { name: "videoFile", maxCount: 1 },
+    { name: "thumbnail", maxCount: 1 },
+  ]),
+  createVideo
+);
 
-videoRouter.post("/createvideo", verifyJWT,
-    upload.fields([
-        { name: "videoFile", maxCount: 1 },
-         { name: "thumbnail", maxCount: 1 },
-    ]),
-    createVideo)
+videoRouter.patch(
+  "/updatevideo/:_id",
+  verifyJWT,
+  upload.fields([
+    { name: "videoFile", maxCount: 1 },
+  ]),
+  updatedVideo
+);
 
-export default videoRouter
+videoRouter.get("/video", getVideo)
+
+
+
+export default videoRouter;
